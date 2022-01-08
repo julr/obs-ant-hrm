87b4fbfb525f6f218addc018c0b7629fda1ad876
Julian <julr@users.noreply.github.com>
2022-01-08 13:10:58 +0100

    Initial commit

---
+++ OBS App/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using Bluegrams.Application;
+using System;
+using System.Timers;
+using System.Windows;
+using System.IO;
+
+namespace ObsHeartRateMonitor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Ant.Dongle antDongle;
+        private readonly Ant.Device.HeartRateMonitor heartRateMonitor;
+        private readonly Timer logTimer;
+        private int lastLogValue;
+        public MainWindow()
+        {
+            PortableSettingsProvider.SettingsFileName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + ".config";
+            PortableSettingsProvider.SettingsDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            PortableSettingsProvider.ApplyProvider(Properties.Settings.Default);
+
+            InitializeComponent();
+
+            Application.Current.DispatcherUnhandledException += UnhandledException;
+
+            int sensor = Properties.Settings.Default.Sensor;
+            if(sensor != 0) TextBoxSensorId.Text = sensor.ToString();
+            CheckBoxLogEnabled.IsChecked = Properties.Settings.Default.IsLogEnabled;
+            ComboBoxLogRate.SelectedIndex = Properties.Settings.Default.LogRefreshRate;
+
+            try
+            {
+                antDongle = new Ant.Dongle();
+                antDongle.Initialize();
+
+                heartRateMonitor = new Ant.Device.HeartRateMonitor(antDongle.Channels[0]);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Close();
+            }
+            
+            logTimer = new Timer
+            {
+                AutoReset = true
+            };
+            logTimer.Elapsed += LogTimer_Elapsed;
+        }
+
+        private void UnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unhandled exception occurred: " + e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            Close();
+        }
+
+        private void ButtonSearch_Click(object sender, RoutedEventArgs e)
+        {
+            var searchWindow = new SearchWindow(heartRateMonitor);
+            searchWindow.ShowDialog();
+        }
+
+        private void ButtonConnectDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            if((string)ButtonConnectDisconnect.Content == "Connect")
+            {
+                Connect();
+            }
+            else
+            {
+                Disconnect();
+            }
+        }
+
+        private void HeartRateMonitor_NewSensorDataReceived(object? sender, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(() => { TextBlockHeartRate.Text = heartRateMonitor.HeartRate.ToString(); }));
+        }
+        private void HeartRateMonitor_SensorNotFound(object? sender, EventArgs e)
+        {
+            MessageBox.Show("Sensor not found", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            Dispatcher.BeginInvoke(new Action(() => { Disconnect(); }));
+        }
+
+        private void LogTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            int heartRate = heartRateMonitor.HeartRate;
+            if((heartRate > 0) && (lastLogValue != heartRate))
+            {
+                File.WriteAllText(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + ".txt", heartRate.ToString());
+                lastLogValue = heartRate;
+            } 
+        }
+
+        private void Connect()
+        {
+            if (int.TryParse(TextBoxSensorId.Text, out int sensorId))
+            {
+                if ((sensorId >= 0) && (sensorId <= 65535))
+                {
+                    ButtonConnectDisconnect.Content = "Disconnect";
+                    TextBoxSensorId.IsEnabled = false;
+                    ButtonSearch.IsEnabled = false;
+                    GroupBoxObs.IsEnabled = false;
+                    heartRateMonitor.NewSensorDataReceived += HeartRateMonitor_NewSensorDataReceived;
+                    heartRateMonitor.SensorNotFound += HeartRateMonitor_SensorNotFound;
+                    heartRateMonitor.Start((ushort)sensorId);
+
+                    if (sensorId > 0)
+                    {
+                        Properties.Settings.Default.Sensor = sensorId;
+                    }
+                    Properties.Settings.Default.IsLogEnabled = CheckBoxLogEnabled.IsChecked ?? false;
+                    Properties.Settings.Default.LogRefreshRate = ComboBoxLogRate.SelectedIndex;
+                    Properties.Settings.Default.Save();
+
+                    if (CheckBoxLogEnabled.IsChecked ?? false)
+                    {
+                        logTimer.Interval = (ComboBoxLogRate.SelectedIndex + 1) * 1000;
+                        logTimer.Start();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Sensor ID must be between 0 (=Any) and 65535");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sensor ID must be a number");
+            }
+        }
+
+        private void Disconnect()
+        {
+            ButtonConnectDisconnect.Content = "Connect";
+            TextBoxSensorId.IsEnabled = true;
+            ButtonSearch.IsEnabled = true;
+            GroupBoxObs.IsEnabled = true;
+            heartRateMonitor.NewSensorDataReceived -= HeartRateMonitor_NewSensorDataReceived;
+            heartRateMonitor.SensorNotFound -= HeartRateMonitor_SensorNotFound;
+            heartRateMonitor.Stop();
+            logTimer.Stop();
+        }
+
+    }
+}
---
+++ OBS App/SearchWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ObsHeartRateMonitor
+{
+    /// <summary>
+    /// Interaction logic for SearchWindow.xaml
+    /// </summary>
+    public partial class SearchWindow : Window
+    {
+        private readonly Ant.Device.HeartRateMonitor heartRateMonitor;
+        public SearchWindow(Ant.Device.HeartRateMonitor heartRateMonitor)
+        {
+            InitializeComponent();
+            this.Closing += SearchWindow_Closing;
+            this.heartRateMonitor = heartRateMonitor;
+            heartRateMonitor.SensorFound += HeartRateMonitor_SensorFound;
+            heartRateMonitor.SensorNotFound += HeartRateMonitor_SensorNotFound;
+            heartRateMonitor.StartSearch();
+        }
+
+        private void HeartRateMonitor_SensorNotFound(object? sender, EventArgs e)
+        {
+            MessageBox.Show("No sensor found", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            Dispatcher.BeginInvoke(new Action(() => { Close(); }));
+        }
+
+        private void HeartRateMonitor_SensorFound(object? sender, ushort e)
+        {
+            Dispatcher.BeginInvoke(new Action(() => { TextBoxSensor.Text = e.ToString(); }));
+        }
+
+        private void SearchWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            heartRateMonitor.Stop();
+        }        
+    }
+}
